faa0d0fec470f88e6ed29d2ecdc910f53aa6d153
jamesrochabrun <jamesrochabrun@gmail.com>
2025-03-03 21:58:33 -0800

    Comments

--- MCPClientChatDemo/MCPClientChat/MCPClientChat/MCP/Clients/GithubMCPClient.swift
+++ MCPClientChatDemo/MCPClientChat/MCPClientChat/MCP/Clients/GithubMCPClient.swift
@@ -16,15 +16,16 @@ final class GIthubMCPClient: MCPLLMClient {
    init() {
       Task {
          do {
-            var customEnv = ProcessInfo.processInfo.environment
-            customEnv["PATH"] = "/opt/homebrew/bin:/usr/local/bin:" + (customEnv["PATH"] ?? "")
+            /// Need to define manually the `env`  to be able to initialize client!!!
+            var env = ProcessInfo.processInfo.environment
+            env["PATH"] = "/opt/homebrew/bin:/usr/local/bin:" + (env["PATH"] ?? "")
             //    customEnv["GITHUB_PERSONAL_ACCESS_TOKEN"] = "YOUR_GITHUB_PERSONAL_TOKEN" // Needed for write operations.
             self.client = try await MCPClient(
                info: .init(name: "GIthubMCPClient", version: "1.0.0"),
                transport: .stdioProcess(
                   "npx",
                   args: ["-y", "@modelcontextprotocol/server-github"],
-                  env: customEnv,
+                  env: env,
                   verbose: true
                ),
                capabilities: .init()
